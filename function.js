6ef972b46b48950c9c021689d0a166f70f2d737e
duccatti <jokesupremo@gmail.com>
2022-11-10 11:09:22 -0300

    first commit

---
+++ function.js
@@ -0,0 +1,15 @@
+const x = 5;
+const y = 7;
+
+function digaola() {
+    console.log("Olá");
+}
+
+function multiplica(a, b) {
+    return a*b;
+}
+
+digaola();
+const resultado = multiplica(x, y);
+
+console.log(resultado)
---
+++ script.js
@@ -0,0 +1,42 @@
+const toDoField = document.querySelector('.to-do-field');
+const toDoSend = document.querySelector('.to-do-send');
+const toDoList = document.querySelector('.to-do-list');
+
+function criali(){
+    const li = document.createElement('li');
+    return li;
+}
+
+function criaButtonClear(){
+    const buttonClear = document.createElement("button");
+    buttonClear.setAttribute('class', 'button-clear');
+    buttonClear.innerText = "Deletar";
+    return buttonClear;
+}
+
+function criarTarefa(li, buttonClear){
+    li.innerText = toDoField.value;
+    li.appendChild(buttonClear);
+    toDoList.appendChild(li);
+}
+
+toDoSend.addEventListener('click', function(e){
+    const li = criali();
+    const buttonClear = criaButtonClear();
+    criarTarefa(li, buttonClear)
+});
+
+toDoField.addEventListener('keyup', function(e){
+    if(e.keyCode === 13){
+        const li = criali();
+        const buttonClear = criaButtonClear();
+        criarTarefa(li, buttonClear)
+    }
+})
+
+addEventListener('click', function(e){
+    const el = e.target;
+    if(el.classList.contains('button-clear')){
+        el.parentElement.remove();
+    }
+});
